c958130372c96770b03631e7d7138fa4908f670e
jdsaeyqo <jdsaeyqi@naver.com>
2021-03-24 11:35:40 +0900

    계산기 UI

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun buttonClicked(view: View) {
+
+    }
+
+    fun clearButtonClicked(view: View) {
+
+    }
+
+    fun resultButtonClicked(view: View) {
+
+    }
+
+    fun historyButtonClicked(view: View) {
+
     }
 }
